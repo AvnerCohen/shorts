2658d7a01f24d1abfee22bcf08a3f41107ab883d
AvnerCohen <israbirding@gmail.com>
2012-09-23 16:13:35 +0200

    fix the tr to avoid adding extra single quote

--- set_heroku_env.sh
+++ set_heroku_env.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 export cur=${PWD##*/}
-heroku config:add `cat ~/.dotfiles/private_data/$cur.zsh | cut -f 2 -d " " | tr "\n" " "`
+heroku config:add `cat ~/.dotfiles/private_data/$cur.zsh | cut -f 2 -d " " | tr "\n" " " | tr -d "'"`
 #heroku config:add
