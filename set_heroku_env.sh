d602c6daa6847726c2b67f9fc7a04b584a33071f
AvnerCohen <israbirding@gmail.com>
2012-09-24 14:34:42 +0200

    Add comment on behavior

--- set_heroku_env.sh
+++ set_heroku_env.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+## this will reuse the private_data folder with the name as current folder and will created the variable on the 
+## equivalent heroku app
 export cur=${PWD##*/}
 heroku config:add `cat ~/.dotfiles/private_data/$cur.zsh | cut -f 2 -d " " | tr "\n" " " | tr -d "'"`
 #heroku config:add
