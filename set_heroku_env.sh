918b14c9db05ed84ab994fda7c8e8299061baf5a
AvnerCohen <israbirding@gmail.com>
2012-09-24 14:34:21 +0200

    Test merge tool

--- set_heroku_env.sh
+++ set_heroku_env.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# so this is some test for merge tool
 export cur=${PWD##*/}
 heroku config:add `cat ~/.dotfiles/private_data/$cur.zsh | cut -f 2 -d " " | tr "\n" " " | tr -d "'"`
 #heroku config:add
